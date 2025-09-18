0503c0669988a387be8b7dcbdb13c1895d9ffb59
Yulian Khomechko <khomechkoyulian@gmail.com>
2025-09-18 20:30:13 +0300

    tests.py refactoring

--- tests.py
+++ tests.py
@@ -1,10 +1,23 @@
 from functions.get_files_info import get_files_info
 
-result_1 = get_files_info("calculator", ".")
-print(result_1)
+def test():
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
 
-result_2 = get_files_info("calculator", "pkg")
-print(result_2)
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
 
-result_3 = get_files_info("calculator", "/bin")
-print(result_3)
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
