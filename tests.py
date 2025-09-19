d9132072412ae0bb65c71882abd1ab20b26c039d
Yulian Khomechko <khomechkoyulian@gmail.com>
2025-09-19 19:59:13 +0300

    write_file implementation

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def test():
     # result = get_files_info("calculator", ".")
@@ -19,21 +20,34 @@ def test():
     # print("Result for '../' directory:")
     # print(result)
 
-    result = get_file_content("calculator", "main.py")
-    print("Result for main.py")
+    # result = get_file_content("calculator", "main.py")
+    # print("Result for main.py")
+    # print(result)
+
+    # result = get_file_content("calculator", "pkg/calculator.py")
+    # print("Result for pkg/calculator.py")
+    # print(result
+    #       )
+    # result = get_file_content("calculator", "/bin/cat")
+    # print("Result for /bin/cat")
+    # print(result)
+
+    # result = get_file_content("calculator", "pkg/does_not_exist.py")
+    # print("Result for pkg/does_not_exist.py")
+    # print(result)
+
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print("Result for lorem.txt")
     print(result)
 
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for pkg/calculator.py")
-    print(result
-          )
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for /bin/cat")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print("Result for pkg/morelorem.txt")
     print(result)
     
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print("Result for pkg/does_not_exist.py")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print("Result for /tmp/temp.txt")
     print(result)
+    
 
 if __name__ == "__main__":
     test()
